f18ab44d49124315da1b21f440fe82131e22e983
Filip Stybel <filip.stybel@justcode.io>
2020-04-02 19:05:27 +0200

    Add next and previous attributes to Heading class for linked list

--- lib/heading.rb
+++ lib/heading.rb
@@ -1,7 +1,10 @@
 class Heading
   attr_reader :title, :level
+  attr_accessor :previous, :next
   def initialize(title:, level: 0)
     @title = title
     @level = level
+    @next = nil
+    @previous = nil
   end
 end
--- spec/lib/heading_spec.rb
+++ spec/lib/heading_spec.rb
@@ -10,4 +10,22 @@ describe Heading do
   it 'has level attribute' do
     expect(subject.level).to eq(0)
   end
+
+  it 'has next attribute' do
+    expect(subject.next).to be_nil
+  end
+
+  it 'can change next attribute' do
+    subject.next = 'foo'
+    expect(subject.next).to eq('foo')
+  end
+
+  it 'has previous attribute' do
+    expect(subject.previous).to be_nil
+  end
+
+  it 'can change previous attribute' do
+    subject.previous = 'foo'
+    expect(subject.previous).to eq('foo')
+  end
 end
