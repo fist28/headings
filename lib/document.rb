55dc918952a5d20ccfeff14eb56afc3a2475bbd7
Filip Stybel <filip.stybel@justcode.io>
2020-04-02 18:52:51 +0200

    Brief of logic generation of headings numeration

--- lib/document.rb
+++ lib/document.rb
@@ -1,26 +1,33 @@
 class Document
   def initialize
+    @headings_numeration = []
     @headings_list = []
   end
 
   def add_heading(title:, level: 0)
+    if @headings_numeration.size.zero?
+      @headings_numeration << Array.new.fill(1, 0, level+1)
+    else
+      previous_numeration =  @headings_numeration[-1]
+
+      if level > previous_numeration.size
+        new_numeration = previous_numeration.dup.fill(1, previous_numeration.size, level)
+      else
+        new_numeration = previous_numeration[0..level]
+        new_numeration[level] += 1
+      end
+      @headings_numeration << new_numeration
+    end
     @headings_list << Heading.new(title: title, level: level)
   end
 
   def to_s
     result = ""
-    space = "\t" * (@headings_list[0].level)
-    result << "#{space}1.\t#{@headings_list[0].title.capitalize}\n"
-
-    space = "\t" * (@headings_list[1].level)
-    result << "#{space}1.1.1.\t#{@headings_list[1].title.capitalize}\n"
-
-    space = "\t" * (@headings_list[2].level)
-    result << "#{space}1.2.\t#{@headings_list[2].title.capitalize}\n"
-
-    space = "\t" * (@headings_list[3].level)
-    result << "#{space}1.3.\t#{@headings_list[3].title.capitalize}\n"
-
+    @headings_list.each_with_index do |heading, index|
+      space = "\t" * (heading.level)
+      number = @headings_numeration[index].join('.') << '.'
+      result << "#{space}#{number}\t#{heading.title.capitalize}\n"
+    end
     result
   end
 end
