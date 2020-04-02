51cc999f38747ecbc7f97b01e2c31996b86fbacd
Filip Stybel <filip.stybel@justcode.io>
2020-04-02 17:13:51 +0200

    Init repository

---
+++ Gemfile
@@ -0,0 +1,3 @@
+gem 'rspec', '~> 3.4', group: [:development, :test]
+gem 'simplecov', require: false, group: :test
+gem 'byebug', group: [:development]
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'simplecov'
+require 'rspec'
+
+SimpleCov.start
+project_root = File.dirname(File.absolute_path(__FILE__))
+Dir.glob(project_root + '/../lib/*.rb') { |file| require file }
