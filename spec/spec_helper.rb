d07314e1f62ae90e2076e59ea43eecd7db21d237
Filip Stybel <filip.stybel@justcode.io>
2020-04-02 18:46:40 +0200

    Add rspec to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'simplecov'
 require 'rspec'
-
+require 'byebug'
 SimpleCov.start
 project_root = File.dirname(File.absolute_path(__FILE__))
 Dir.glob(project_root + '/../lib/*.rb') { |file| require file }
